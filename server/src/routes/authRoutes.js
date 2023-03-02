159dbc839da311c45451c94ed97b3f142784b5e1
ausrys <ausryszz1@gmail.com>
2023-03-02 16:54:28 +0200

    apsitvarkymas

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,7 +4,7 @@ const {requireAuth} = require('../middleware/authMiddleware')
 const router = Router();
 router.get('/', authController.home_get);
 router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
+router.post('/signup', authController.signup_post);  
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
 router.get('/logout', authController.logout_get);
